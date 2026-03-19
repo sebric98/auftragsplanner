f73d8707eceb13b2db13707cc13d45f0bf099ac9
seric103 <seric103@hhu.de>
2026-03-19 20:09:26 +0100

    init project

---
+++ src/main/java/sebastianrichter/auftragsplanner/AuftragsPlannerApplication.java
@@ -0,0 +1,13 @@
+package sebastianrichter.auftragsplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuftragsPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuftragsPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sebastianrichter/auftragsplanner/AuftragsPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package sebastianrichter.auftragsplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuftragsPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
